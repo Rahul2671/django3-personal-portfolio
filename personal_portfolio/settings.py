a9c36d0d700477648f06f218c647600a650d9c43
Rahul2671 <rahulsubha1983@gmail.com>
2025-07-20 18:49:51 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&(&8$*a4850tyinz8g&7a=7&31)((wscqf4-fvzu$n50w7v6s3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rahulr26.pythonanywhere.com', 'www.rahulr26.pythonanywhere.com','https://django3-personal-portfolio.onrender.com']
+ALLOWED_HOSTS = ['rahulr26.pythonanywhere.com', 'www.rahulr26.pythonanywhere.com', 'django3-personal-portfolio.onrender.com']
 
 
 # Application definition
