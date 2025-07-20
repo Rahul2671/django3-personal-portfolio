bf416cc010d399285628965bfb3343865e2f4bff
Rahul2671 <rahulsubha1983@gmail.com>
2025-07-20 18:54:55 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,14 +120,12 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
-STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 
 try:
     from .local_settings import *
